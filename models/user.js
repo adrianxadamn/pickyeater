502622bcd5b093584502734252aa3fc0a892dfd4
Adrian Nuyda <adamnuyda@yahoo.com>
2016-03-13 12:45:02 -0700

    adds models

--- models/user.js
+++ models/user.js
@@ -2,8 +2,10 @@ var mongoose = require('mongoose'),
     debug    = require('debug')('app:models');
 
 var userSchema = new mongoose.Schema({
-  name:   String,
-  handle: String
+  googledId:   { type: String, required: true },
+  googleImage: { type: String, required: true },
+  name:        { type: String, required: true },
+  email:       { type: String, required: true }
 });
 
 var User = mongoose.model('User', userSchema);
