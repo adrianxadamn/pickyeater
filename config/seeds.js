a4258c1369a7dd55979a5ff1a755edfce4c9ae48
Jerry Ngov <jcngov@yahoo.com>
2016-03-13 17:59:46 -0700

    make seeds for user and wishlist

--- config/seeds.js
+++ config/seeds.js
@@ -1,29 +1,46 @@
 var mongoose = require('./database');
 
 var User = require('../models/user');
+var Wishlist = require('../models/wishlist');
 
-var users = [
-  { // 0
-    handle: "DunkLord",
-    name:   "Bob Neverdunk"
-  },
-  { // 1
-    handle: "MoneyMarge",
-    name:   "Margaret Kalanchoe"
-  }
-];
-
-User.remove({}, function(err) {
-  if (err) console.log(err);
-  User.create(users, function(err, users) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Database seeded with " + users.length  + " users.");
-      mongoose.connection.close(function(err) {
-        if (err) console.log(err);
-        process.exit(0);
-      });
-    }
+User.remove({}, function(err, users) {
+  Wishlist.remove({}, function(err, users) {
+    User.create(
+      [
+        { // 0
+          name:   "Bob Neverdunk",
+          googledId: "abc123",
+          googleImage: "myprofilepic.img",
+          email: "bob@email.com"
+        },
+        { // 1
+          name:   "Margaret Kalanchoe",
+          googledId: "xyz123",
+          googleImage: "myprofilepic.png",
+          email: "marg@email.com"
+        }
+      ], function(err, users) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(users[0].name);
+          Wishlist.create([
+            {title: "List1", creator: users[0]},
+            {title: "List2", creator: users[0]},
+            {title: "List3", creator: users[1]}
+          ],
+         function(err, wishlists) {
+            wishlists[0].restaurants.push({name: "Claws", address:"df", url: "dfs", pricing: 2, cuisine: "Seafood", picture_url: "dfa", diet_restriction: "df", rating: 2, rating_img_url: "dfd"});
+            console.log(wishlists[0]);
+            if (err) {
+              console.log(err);
+            } else {
+              console.log(wishlists);
+            }
+          });
+        }
+    });
   });
 });
+
+
