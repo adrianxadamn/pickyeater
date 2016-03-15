b75e26f44706bdcd68b480a8a3c202d3a9037ad3
Jerry Ngov <jcngov@yahoo.com>
2016-03-14 17:50:57 -0700

    rendering wishlist to html

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,41 @@
 console.log('JS loaded!');
+
+$( document ).ready(function() {
+  var $wishlists = $('#wishlist-list');
+  var $title = $('#title');
+
+  $.ajax({
+    type: 'GET',
+    url: '/api/wishlists',
+    success: function(wishlists) {
+      $.each(wishlists, function(i, wishlist) {
+        console.log(wishlist);
+        $wishlists.append(`<li> ${wishlist.title} </li> <li> ${wishlist.creator} </li>` );
+      });
+    },
+    error: function(err) {
+      console.log(err);
+    }
+  });
+
+  $('#add-wishlist').on('click', function() {
+
+    var wishlist = {
+      title: $title.val()
+    };
+
+    $.ajax({
+      type: 'POST',
+      url: '/api/wishlists',
+      data: wishlist,
+      success: function(newWishlist) {
+        $wishlists.append(`<li> ${wishlist.title} </li> <li> ${wishlist.creator} </li>` );
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    })
+  })
+});
+
+
