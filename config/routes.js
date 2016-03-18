e2ceb1d9d2d227fb9aad2b9cf04baa3149a9293b
Adrian Nuyda <adamnuyda@yahoo.com>
2016-03-17 22:45:15 -0700

    adds api wishlist controller to make code cleaner

--- config/routes.js
+++ config/routes.js
@@ -6,25 +6,25 @@ var express = require('express'),
 var usersController = require('../controllers/users');
 var searchController = require('../controllers/search');
 var wishlistController = require('../controllers/wishlists');
-
+var wishlistApiController = require('../controllers/api_wishlists');
 //////////////////////////////////////////////////
 ////////////////////root path/////////////////////
 //////////////////////////////////////////////////
 router.get('/', function(req, res, next) {
   res.render('home', {user: req.user})
-})
+});
 //////////////////////////////////////////////////
 /////////////search restaurant path///////////////
 //////////////////////////////////////////////////
 router.get('/restaurants', function(req, res, next) {
   res.render('restaurants', {user: req.user});
-})
+});
 //////////////////////////////////////////////////
 /////////////////wishlists path://////////////////
 //////////////////////////////////////////////////
 router.get('/wishlists', function(req, res, next) {
   res.render('wishlists', {user: req.user});
-})
+});
 router.get('/wishlists/:id', wishlistController.show);
 router.get('/wishlists/:id/random', wishlistController.randomRest);
 router.put('/wishlists/:id', wishlistController.listUpdate);
@@ -32,14 +32,13 @@ router.delete('/wishlists/:id', wishlistController.destroy);
 //////////////////////////////////////////////////
 ///////////////wishlists api path:////////////////
 //////////////////////////////////////////////////
-router.get('/api/wishlists', wishlistController.index);
-router.get('/api/wishlists/:id', wishlistController.showAPI);
-router.put('/api/wishlists/:id', wishlistController.addRestaurant)
-router.put('/api/wishlists/:id/restaurants', wishlistController.addRestaurant);
-router.post('/api/wishlists', wishlistController.post);
-router.get('/api/wishlists/:id/random', wishlistController.randomRest);
-router.delete('/api/wishlists/:wl_id/restaurants/:rs_id', wishlistController.removeRestaurant);
-
+router.get('/api/wishlists', wishlistApiController.index);
+router.get('/api/wishlists/:id', wishlistApiController.showAPI);
+router.put('/api/wishlists/:id', wishlistApiController.addRestaurant)
+router.put('/api/wishlists/:id/restaurants', wishlistApiController.addRestaurant);
+router.post('/api/wishlists', wishlistApiController.post);
+router.get('/api/wishlists/:id/random', wishlistApiController.randomRest);
+router.delete('/api/wishlists/:wl_id/restaurants/:rs_id', wishlistApiController.removeRestaurant);
 //////////////////////////////////////////////////
 ////////////// search resource paths//////////////
 //////////////////////////////////////////////////
